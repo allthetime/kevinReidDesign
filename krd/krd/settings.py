ce2866ab161d7904f1267ae9a1520f7c971144f1
Christopher Reid <chronic88@gmail.com>
2015-01-22 00:42:06 -0800

    static etc got the skeleton

--- krd/krd/settings.py
+++ krd/krd/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates').replace('\\','/'),
+)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -84,5 +88,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = ("/Users/allthetime/kevin/krd/static/portfolio",)
+
+
+
